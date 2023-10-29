95d4ba8c4536dd868b816feed22e52330bf174b5
nurshahirah <42369006+iraazahari@users.noreply.github.com>
2023-10-30 02:54:16 +0800

    Add files via upload

---
+++ TG Malaysia/SQLQuery6.sql
@@ -0,0 +1,10 @@
+--SELECT * FROM freelancers
+
+
+--INSERT INTO Freelancers (id, username, mail, phoneNumber, skillsets, hobby)
+--VALUES ('4','john_doe', 'john.doe@example.com', '123-456-7890', 'Web Development', 'Gardening');
+
+--DELETE FROM Freelancers
+--WHERE id = '1';
+
+
